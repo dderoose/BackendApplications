c778983c7136405a1f7c7055b52b0708d0f3722b
dderoose <dennis.deroose.w0495@student.hogent.be>
2018-12-26 23:02:30 +0100

    test

---
+++ BackendAuth/Migrations/201812261608053_Update.cs
@@ -0,0 +1,195 @@
+namespace BackendAuth.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Breaks",
+                c => new
+                    {
+                        BreakId = c.Int(nullable: false, identity: true),
+                        Player = c.String(),
+                        MomentPlayed = c.DateTime(nullable: false),
+                        NumberPoints = c.Int(nullable: false),
+                        Opponent = c.String(),
+                        TypeBreak = c.String(),
+                        FrameId = c.Int(),
+                        User_UserId = c.Int(),
+                    })
+                .PrimaryKey(t => t.BreakId)
+                .ForeignKey("dbo.Users", t => t.User_UserId)
+                .ForeignKey("dbo.Frames", t => t.FrameId)
+                .Index(t => t.FrameId)
+                .Index(t => t.User_UserId);
+            
+            CreateTable(
+                "dbo.Frames",
+                c => new
+                    {
+                        FrameId = c.Int(nullable: false, identity: true),
+                        DurationFrame = c.DateTime(nullable: false),
+                        Points = c.Int(nullable: false),
+                        Winner = c.String(),
+                        MatchId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.FrameId)
+                .ForeignKey("dbo.Matches", t => t.MatchId, cascadeDelete: true)
+                .Index(t => t.MatchId);
+            
+            CreateTable(
+                "dbo.Matches",
+                c => new
+                    {
+                        MatchId = c.Int(nullable: false, identity: true),
+                        PotSucces = c.Double(nullable: false),
+                        SafetySucces = c.Double(nullable: false),
+                        DurationMatch = c.DateTime(nullable: false),
+                        AverageShotTime = c.DateTime(nullable: false),
+                        NumberMatchesWon = c.Int(nullable: false),
+                        UserId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.MatchId)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.Int(nullable: false, identity: true),
+                        Username = c.String(),
+                        Paswoord = c.String(),
+                        PaswoordConfirm = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                        IdentityUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.IdentityUsers", t => t.IdentityUser_Id)
+                .Index(t => t.RoleId)
+                .Index(t => t.IdentityUser_Id);
+            
+            CreateTable(
+                "dbo.IdentityUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                        IdentityUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.IdentityUsers", t => t.IdentityUser_Id)
+                .Index(t => t.IdentityUser_Id);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        IdentityUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.IdentityUsers", t => t.IdentityUser_Id)
+                .Index(t => t.IdentityUser_Id);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        FirstName = c.String(nullable: false, maxLength: 100),
+                        LastName = c.String(nullable: false, maxLength: 100),
+                        Password = c.String(nullable: false),
+                        ConfirmPassword = c.String(nullable: false),
+                        Level = c.Byte(nullable: false),
+                        JoinDate = c.DateTime(nullable: false),
+                        LastTimeVisitNotifications = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.IdentityUsers", t => t.Id)
+                .Index(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUsers", "Id", "dbo.IdentityUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "IdentityUser_Id", "dbo.IdentityUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "IdentityUser_Id", "dbo.IdentityUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "IdentityUser_Id", "dbo.IdentityUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Breaks", "FrameId", "dbo.Frames");
+            DropForeignKey("dbo.Frames", "MatchId", "dbo.Matches");
+            DropForeignKey("dbo.Matches", "UserId", "dbo.Users");
+            DropForeignKey("dbo.Breaks", "User_UserId", "dbo.Users");
+            DropIndex("dbo.AspNetUsers", new[] { "Id" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "IdentityUser_Id" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "IdentityUser_Id" });
+            DropIndex("dbo.IdentityUsers", "UserNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "IdentityUser_Id" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Matches", new[] { "UserId" });
+            DropIndex("dbo.Frames", new[] { "MatchId" });
+            DropIndex("dbo.Breaks", new[] { "User_UserId" });
+            DropIndex("dbo.Breaks", new[] { "FrameId" });
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.IdentityUsers");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Users");
+            DropTable("dbo.Matches");
+            DropTable("dbo.Frames");
+            DropTable("dbo.Breaks");
+        }
+    }
+}
