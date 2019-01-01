09de59de8db416c9d92bf4bad29483810b6a4b6b
dderoose <dennis.deroose.w0495@student.hogent.be>
2019-01-01 15:20:31 +0100

    Added errors in dutch

---
+++ BackendAuth/Migrations/201901011420059_ChangeErrors.cs
@@ -0,0 +1,16 @@
+namespace BackendAuth.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangeErrors : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
